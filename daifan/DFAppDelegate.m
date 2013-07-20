3622653524dbdbaf90f01304f59d33607511da0b
Cyril Wei <teakscn@gmail.com>
2013-07-20 19:05:25 +0800

    [cyril] added Baidu push library.

--- daifan/DFAppDelegate.m
+++ daifan/DFAppDelegate.m
@@ -1,6 +1,8 @@
 #import "DFAppDelegate.h"
 #import "DFSplashViewController.h"
 #import "AFNetworkActivityIndicatorManager.h"
+#import "BPush.h"
+#import "JSONKit.h"
 
 @implementation DFAppDelegate
 
@@ -15,7 +17,41 @@
 
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
+    
+    [BPush setupChannel:launchOptions];
+    [BPush setDelegate:self];
+    //[BPush setAccessToken:@"3.536568bf6a653bcf373879f98250f0f9.2592000.1369200738.282335-313751"];
+    
+    [application setApplicationIconBadgeNumber:1];
+    [application registerForRemoteNotificationTypes:
+     UIRemoteNotificationTypeAlert
+     | UIRemoteNotificationTypeBadge
+     | UIRemoteNotificationTypeSound];
+    
     return YES;
 }
+
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
+    
+    NSLog(@"deviceToken:%@",deviceToken);
+    [BPush registerDeviceToken: deviceToken];
+}
+
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo {
+    NSLog(@"Receive Notify: %@", [userInfo JSONString]);
+    NSString *alert = [[userInfo objectForKey:@"aps"] objectForKey:@"alert"];
+    if (application.applicationState == UIApplicationStateActive) {
+        UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Did receive a Remote Notification"
+                                                            message:[NSString stringWithFormat:@"The application received this remote notification while it was running:\n%@", alert]
+                                                           delegate:self
+                                                  cancelButtonTitle:@"OK"
+                                                  otherButtonTitles:nil];
+        [alertView show];
+    }
+    [application setApplicationIconBadgeNumber:0];
+    
+    [BPush handleNotification:userInfo];
+}
+
 
 @end
